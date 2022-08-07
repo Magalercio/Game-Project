61cef4113b12836695f4ced4af0f352254230286
magalercio <magalercio.gui@gmail.com>
2022-08-07 13:09:47 -0300

    criando a tela
    Definido a função que desenha todos os elementos na tela

--- FlappyBird.py
+++ FlappyBird.py
@@ -160,3 +160,18 @@ class Chao:
     def desenhar(self, tela):
         tela.blit(self.IMAGEM, (self.x1, self.y))
         tela.blit(self.IMAGEM, (self.x2, self.y))
+
+
+def desenhar_tela(tela, passaros, canos, chao, pontos):
+    tela.blit(IMAGEM_BACKGROUND,(0,0))
+    for passaro in passaros:
+        passaro.desenhar(tela)
+    for cano in canos:
+        cano.desenhar(tela)
+
+    texto = FONTE_PONTES.render(f"Pontuação: {pontos}", 1, (255, 255, 255))
+    tela.blit(texto, (TELA_LARGURA - 10 - texto_get_width()))
+    chao.desenhar(tela)
+    pygame.display.update()
+
+
